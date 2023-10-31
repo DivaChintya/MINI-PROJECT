c3fa8bf046b5b52215372e11693192d7d516f812
DivaChintya <149126928+DivaChintya@users.noreply.github.com>
2023-10-31 22:05:27 +0700

    db.js

--- db.js
+++ db.js
@@ -2,9 +2,11 @@ const mysql = require('mysql'); //panggil library mysql
 
 const db = mysql.createPool({ //keperluan untuk koneksi db
     host: 'localhost', //KONFIGURASI nya
-    user: 'root', //tergantung user konfigurasi 
+    user: 'root', 
     password: '',
     database: 'negara_asia' //nama db
 });   
 
 exports.db = db; //
+
+//UNTUK FILE db.js SAYA MASUKAN DI FOLDER MODEL >>>>>>>>>>>>>>>>>>>>
